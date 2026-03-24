f94636e4ac168650115f54b4f167645fb82793ab
kanikashreesivakumar <kanikashreesivakumar16@gmail.com>
2026-03-24 12:21:15 +0530

    page.js in main frontend file

---
+++ frontend/app/page.js
@@ -0,0 +1,47 @@
+"use client";
+
+import { useState} from "react";
+
+export default function Home(){
+    const [file ,setFile] = useState(null);
+    const [result ,setResult]= useState("");
+    const [loading ,setLoading]= useState(false);
+
+    const uploadFile = async () => {
+        if(!file) return;
+
+        const formData = new FormData();;
+        formData.append("file",file);
+
+        setLoading(true);
+
+        try{
+            const res =await fetch("http://localhost:5000/upload",{
+                methos: "POST",
+                body: formData,
+            });
+
+            const data = await res.json();
+            setResult(data.analysis);
+
+        }catch (err){
+            setResult("Error occured");
+        }
+
+        setLoading(false);
+    };
+
+    return (
+        <div style = {{ padding:30}}>
+            <h1> AI Resume Analyzer</h1>
+
+            <input type = "file" onChange={(e)=> setFile(e.target.files[0])}/>
+
+            <button onClick ={uploadFile}> Upload</button>
+
+            {loading && <p>Analyzing...</p>}
+
+            <pre>{result}</pre>
+        </div>
+    );
+}
